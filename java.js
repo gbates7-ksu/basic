5aab7ef09eacc7589c39d327a1347a341bd97a9a
gbates7-ksu <gbates7@students.kennesaw.edu>
2024-06-15 21:10:13 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+var body = document.getElementById("body");
+		body.style.textAlign = "center";
+		
+		alert("Go Braves");
+		
+		var menu = document.getElementById("menu");
+		menu.style.backgroundColor = "#D3D3D3";
